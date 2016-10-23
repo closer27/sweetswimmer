8be97c417738c6f6519dab1538ba35546a70ee38
Seungwon Kim <closer527@gmail.com>
2016-10-23 23:57:05 +0900

    apply previous theme temporarily

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,6 @@ import './public/css/styles.css';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './public/css/agency.css', './public/css/font-awesome.min.css']
 })
 export class AppComponent { }
